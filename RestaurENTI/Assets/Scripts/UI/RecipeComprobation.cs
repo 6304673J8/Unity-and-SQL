b1d9c023ff73579ad531bbe29b7d98023be8b84e
Geo <petergeorghe.julca@enti.cat>
2022-02-18 20:49:46 +0100

    CleanUp Files V1

--- RestaurENTI/Assets/Scripts/UI/RecipeComprobation.cs
+++ RestaurENTI/Assets/Scripts/UI/RecipeComprobation.cs
@@ -33,89 +33,13 @@ public class RecipeComprobation : MonoBehaviour, IPointerClickHandler
 
         text = recipeTitleText.GetComponent<Text>();
         nameRecipe = text;
-
+        
         //CheckIngredients();
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        //recipeBook.SetActive(false);
         audioData.Play(0);
         Debug.Log("Clicked: " + eventData.pointerCurrentRaycast.gameObject.name);
-        //ComprobateState();
     }
-/*
-
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        recipeBook.SetActive(false);
-        audioData.Play(0);
-        Debug.Log("Clicked: " + eventData.pointerCurrentRaycast.gameObject.name);
-        ComprobateState();
-    }
-
-    public void ComprobateState()
-    {
-        if (isShowing == false)
-        {
-            recipeCanvas.SetActive(true);
-            recipeBook.SetActive(false);
-            isShowing = true;
-        }
-        else if (isShowing == true)
-        {
-            recipeCanvas.SetActive(false);
-            recipeBook.SetActive(true);
-            isShowing = false;
-        }
-    }
-    public void ListIngredients()
-    {
-        List<RecipeIngredients> recipeIngredients = new List<RecipeIngredients>();
-        List<Recipe> recipes = new List<Recipe>();
-        List<Ingredient> ingredients = new List<Ingredient>();
-
-        recipeIngredients = database.GetRecipeIngredients();
-        recipes = database.GetRecipes();
-        ingredients = database.GetIngredients();
-
-        for (int i = 0; i < recipes.Count; i++)
-        {
-            idRecipe = recipes[i].id_recipe;
-
-            if (recipes[i].recipe == nameRecipe.text)
-            {
-                confirmRecipeId = idRecipe;
-                Debug.Log(idRecipe + (". ") + recipes[i].recipe);
-            }
-        }
-
-        for (int i = 0; i < recipeIngredients.Count; i++)
-        {
-            if (confirmRecipeId == recipeIngredients[i].id_recipe)
-            {
-                PrintIngredients(recipeIngredients[i].id_ingredient);
-            }
-        }
-    }
-
-    public void PrintIngredients(int idIngredient)
-    {
-        List<Ingredient> ingredients = new List<Ingredient>();
-
-        ingredients = database.GetIngredients();
-        for (int i = 0; i < ingredients.Count; i++)
-        {
-            GameObject ingredient;
-            Text text;
-
-            if (ingredients[i].id_ingredient == idIngredient)
-            {
-                ingredient = Instantiate(RecipeIngredient, posToInstantiate);
-                text = ingredient.GetComponentInChildren<Text>();
-
-                text.text = ingredients[i].ingredient;
-            }
-        }
-    }*/
 }
